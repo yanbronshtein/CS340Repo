8e24402407efcd4910f130119fe8440541e1728f
Yaniv Bronshtein <yanbronshtein@gmail.com>
2020-03-01 18:49:09 -0500

    Edit 6.49pm

--- zombie.c
+++ zombie.c
@@ -7,34 +7,20 @@
 #include <stdio.h>
 int main() 
 { 
-    // Fork returns process id 
-    // in parent process 
-    int pid = fork(); 
+  // Fork returns process id 
+  // in parent process 
+  int pid = fork(); 
   
-    // Parent process  
-    if (pid > 0) {
-        printf("I am the parent and my id is %d\n",getpid());
-        // sleep(200); //Force parent to sleep for 200 seconds so child can terminate first
-        // printf("Parent Finished execution with PID: %d\n",getpid());
-
-    }
-  
-    // Child process 
-    else {        
-        printf("I am the child with pid %d and my parent has ppid %d\n",getpid(),getppid());
-        // for (int i = 0; i < 10; i++)
-        // {
-        //   printf("From Child with PID:%d\n",getpid());
-        //   printf("Count: %d\n",i);
-        //   sleep(1);
-          
-        // }
-
-        sleep(1);
-        
-        printf("Exiting from Child with PID: %d\n",getpid());
-        exit(0); 
-    }
-  
-    return 0; 
+  // Parent process  
+  if (pid > 0) {
+    printf("I am the parent and my id is %d\n",getpid());
+    sleep(200); //Force parent to sleep for 200 seconds so child can terminate first and turn into a zombie
+  }
+  // Child process 
+  else {        
+    printf("I am the child with pid %d and my parent has ppid %d\n",getpid(),getppid());
+    sleep(1);
+    exit(0); 
+  }
+  return 0; 
 } 
